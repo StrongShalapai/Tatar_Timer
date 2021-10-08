9ba24cc81909ed178facb29b081c4d77f419b0ca
Ilshat <92148098+StrongShalapai@users.noreply.github.com>
2021-10-08 15:06:41 +0400

    commit

---
+++ app/src/main/java/com/example/tatar_timer/stopwatch.java
@@ -0,0 +1,14 @@
+package com.example.tatar_timer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class stopwatch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+}
