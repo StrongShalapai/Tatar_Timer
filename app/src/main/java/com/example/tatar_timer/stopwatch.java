0ff45423666ee4da6e86d98592d34746c3f8b85f
Ilshat <92148098+StrongShalapai@users.noreply.github.com>
2021-10-08 15:15:09 +0400

    Update stopwatch.java

--- app/src/main/java/com/example/tatar_timer/stopwatch.java
+++ app/src/main/java/com/example/tatar_timer/stopwatch.java
@@ -3,11 +3,16 @@ package com.example.tatar_timer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class stopwatch extends AppCompatActivity {
+    private Button btn_goToStopwatch;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        btn_goToStopwatch = findViewById(R.id.btn_stopwatch);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
     }
