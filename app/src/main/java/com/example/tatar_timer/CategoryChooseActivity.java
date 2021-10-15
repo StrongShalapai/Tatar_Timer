daf0cca13f103af2c6ea116086e99f5f132dcabd
Ilshat <92148098+StrongShalapai@users.noreply.github.com>
2021-10-15 14:28:41 +0400

    stopwatch Как я заебался

--- app/src/main/java/com/example/tatar_timer/CategoryChooseActivity.java
+++ app/src/main/java/com/example/tatar_timer/CategoryChooseActivity.java
@@ -5,47 +5,89 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.tatar_timer.adapters.CategoryAdapter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CategoryChooseActivity extends AppCompatActivity {
 
     private RecyclerView categoryList;
     private CategoryAdapter categoryAdapter;
+    private Button button_commit;
+    private EditText editText;
+    private RecyclerView recyclerView;
     final String SAVED_CATEGORY = "saved_category";
     SharedPreferences preferences;
+    private static final String TAG = "myLogs";
+    Context context;
+
+    private String newCater;
+    private List<String> data;
+
+
+    private void toaster(String text) {
+        Toast toast = Toast.makeText(getApplicationContext(),
+                text, Toast.LENGTH_SHORT);
+        toast.show();
+    }
 
 
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        context = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+        button_commit = findViewById(R.id.btn_commitNewCategory);
+        editText = findViewById(R.id.et_newCategory);
 
 
         Bundle receivedCategories = getIntent().getExtras();
         String[] categories = receivedCategories.getStringArray("categoryArray");
+        ArrayList<String> updatedList = new ArrayList<>(Arrays.asList(categories));
+//        data = new ArrayList<String>();
+        data = Arrays.asList(receivedCategories.getStringArray("categoryArray"));
+
 
         categoryList = findViewById(R.id.rc_names);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
 
         //создаем столько вью сколько есть всего категорий
-        categoryAdapter = new CategoryAdapter(categories.length, categories);
-
+        categoryAdapter = new CategoryAdapter(this, categories.length, updatedList); //Наш адаптер
+//
         categoryList.setLayoutManager(layoutManager);
-        categoryList.setHasFixedSize(true);
+        categoryList.setHasFixedSize(false);
         categoryList.setAdapter(categoryAdapter);
 
 
+//        ArrayList<String> updatedList = new ArrayList<String>(categories.);
 
+        View.OnClickListener commitAndUpdate = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updatedList.add(updatedList.size(), editText.getText().toString());
+                categoryAdapter.notifyItemInserted(updatedList.size());
+                Log.d(TAG, editText.getText().toString() + "\n" + updatedList.size());
+                categoryList.setAdapter(new CategoryAdapter(context, categories.length, updatedList));
+                categoryList.invalidate();
+            }
+        };
+
+        button_commit.setOnClickListener(commitAndUpdate);
 
     }
-
 
 
 }
--- app/src/main/java/com/example/tatar_timer/adapters/CategoryAdapter.java
+++ app/src/main/java/com/example/tatar_timer/adapters/CategoryAdapter.java
@@ -1,25 +1,48 @@
 package com.example.tatar_timer.adapters;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.nfc.Tag;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tatar_timer.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.Context.MODE_PRIVATE;
+
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> {
     private int numberItems;
+    private LayoutInflater mInflater;
     private static int ViewHolderCount;
     private String[] categories;
+    private ArrayList<String> categoryList;
+    private static final String TAG = "myLogs";
+    private static final String DIVIDER = "|||||||||||||||||||||||";
 
-    public CategoryAdapter(int numberItems, String[] array) {
-        this.numberItems = numberItems;
+    private Boolean flag = true;
+    Context mContext;
+    SharedPreferences sPref;
+    private int mPosition;
+
+    final String SAVED_TEXT = "saved_text";
+
+    public CategoryAdapter(Context context, int numberItems, ArrayList<String> array) {
+//        this.numberItems = numberItems;
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
         ViewHolderCount = 0;
-        categories = array;
+        categories = array.toArray(new String[0]);
+        categoryList = array;
     }
 
     @NonNull
@@ -28,23 +51,27 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
         Context context = parent.getContext();
         int layoutIdForListItem = R.layout.category_number;
 
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View view = inflater.inflate(layoutIdForListItem, parent, false);
+        View view = mInflater.inflate(R.layout.category_number, parent, false);
         CategoryViewHolder viewHolder = new CategoryViewHolder(view);
 
-        viewHolder.itemName.setText("view holder index" + ViewHolderCount);
+//        viewHolder.itemName.setText("view holder index" + ViewHolderCount);
         ViewHolderCount++;
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(CategoryAdapter.CategoryViewHolder holder, int position) {
-        holder.bind(position);
+        mPosition = position;
+        Log.d(DIVIDER, "Divide i say");
+        Log.d(TAG, "Current position is equal" +  String.valueOf(mPosition));
+
+        holder.bind(mPosition);
     }
 
     @Override
     public int getItemCount() {
-        return numberItems;
+        return categories.length;
+//        return numberItems;
     }
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -57,14 +84,49 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
 
             itemName = itemView.findViewById(R.id.tv_holder_for_names);
 
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int positionIndex = getAdapterPosition() + 1;
+                    Toast toast;
+
+                    toast = Toast.makeText(mContext, "Айди этого элемента равно " + positionIndex, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            });
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+
+                    Log.d(DIVIDER, "");
+                    Log.d(TAG, "Зажалось");
+
+                    int positionIndex = getAdapterPosition() ;
+                    Log.d(TAG, "Размер массива " + String.valueOf(categoryList.size()));
+                    Log.d(TAG, "Выбранный элемент " + String.valueOf(positionIndex));
+
+
+                    for (int i = 0; i < categoryList.size(); i ++){
+                        Log.d(TAG, "Выбранный элемент " + categoryList.toArray()[i]);
+                    }
+                    categoryList.remove(positionIndex);
+                    notifyItemRemoved(positionIndex);
+                    notifyItemRangeChanged(positionIndex, categoryList.size());
+
+                    for (int i = 0; i < categoryList.size(); i ++){
+                        Log.d(TAG, "Выбранный элемент " + categoryList.toArray()[i]);
+                    }
+                    flag = false;
+                    return true;
+                }
+            });
+
         }
 
         void bind(int listIndex) {
-//            itemName.setText(S);
-            itemName.setText(categories[listIndex]);
-
+            itemName.setText(categoryList.get(listIndex));
         }
+
     }
-
-
 }
